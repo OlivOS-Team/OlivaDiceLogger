66d3d7d2d62a66c3df19fac0b57d200fd80c13c1
仑质 <benzenpenxil@foxmail.com>
2022-03-12 16:03:50 +0800

    Update data.py

--- OlivaDiceLogger/data.py
+++ OlivaDiceLogger/data.py
@@ -16,7 +16,7 @@ _  / / /_  /  __  / __ | / /__  /| |_  / / /__  / _  /    __  __/
 
 
 OlivaDiceLogger_ver = '3.0.3'
-OlivaDiceLogger_svn = 10
+OlivaDiceLogger_svn = 11
 OlivaDiceLogger_ver_short = '%s(%s)' % (str(OlivaDiceLogger_ver), str(OlivaDiceLogger_svn))
 
 dataPath = './plugin/data/OlivaDice/unity'
