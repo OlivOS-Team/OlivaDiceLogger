90b81f6b586b43ae0eb518d7873e52dc08947838
仑质 <benzenpenxil@foxmail.com>
2022-08-26 17:19:42 +0800

    集中处理用户模态

--- OlivaDiceLogger/msgReply.py
+++ OlivaDiceLogger/msgReply.py
@@ -36,6 +36,7 @@ def unity_reply(plugin_event, Proc):
     dictStrCustom = OlivaDiceCore.msgCustom.dictStrCustomDict[plugin_event.bot_info.hash]
     dictGValue = OlivaDiceCore.msgCustom.dictGValue
     dictTValue.update(dictGValue)
+    dictTValue = OlivaDiceCore.msgCustomManager.dictTValueInit(plugin_event, dictTValue)
 
     replyMsg = OlivaDiceCore.msgReply.replyMsg
     replyMsgLazyHelpByEvent = OlivaDiceCore.msgReply.replyMsgLazyHelpByEvent
