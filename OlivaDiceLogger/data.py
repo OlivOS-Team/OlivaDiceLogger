a1ed86133cde3a99d2264d0791b4e8ad08212bca
Desom-fu <desom233@outlook.com>
2025-12-25 19:35:08 +0800

    3.0.28 优化新增的默认回复

--- OlivaDiceLogger/data.py
+++ OlivaDiceLogger/data.py
@@ -15,8 +15,8 @@ _  / / /_  /  __  / __ | / /__  /| |_  / / /__  / _  /    __  __/
 '''
 
 
-OlivaDiceLogger_ver = '3.0.27'
-OlivaDiceLogger_svn = 35
+OlivaDiceLogger_ver = '3.0.28'
+OlivaDiceLogger_svn = 36
 OlivaDiceLogger_ver_short = '%s(%s)' % (str(OlivaDiceLogger_ver), str(OlivaDiceLogger_svn))
 
 dataPath = './plugin/data/OlivaDice/unity'
