49b7ae746aab80c009ef36c1ec551ebaf57a4aac
Desom-fu <desom233@outlook.com>
2026-08-06 13:29:16 +0800

    fix(logger): 3.0.44 修复日志异常字符崩溃

--- OlivaDiceLogger/data.py
+++ OlivaDiceLogger/data.py
@@ -14,8 +14,8 @@ _  / / /_  /  __  / __ | / /__  /| |_  / / /__  / _  /    __  __/
 @Desc      :   None
 """
 
-OlivaDiceLogger_ver = '3.0.43'
-OlivaDiceLogger_svn = 51
+OlivaDiceLogger_ver = '3.0.44'
+OlivaDiceLogger_svn = 52
 OlivaDiceLogger_ver_short = '%s(%s)' % (str(OlivaDiceLogger_ver), str(OlivaDiceLogger_svn))
 
 dataPath = './plugin/data/OlivaDice/unity'
